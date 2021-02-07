cd3b5e7222ab1ff33015dd357a8ad479d6c27e7a
Arthur Monici <61995846+tujmon@users.noreply.github.com>
2021-02-07 11:54:03 -0300

    a

---
+++ src/main/java/com/example/trabalho4/Trabalho4Application.java
@@ -0,0 +1,13 @@
+package com.example.trabalho4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalho4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trabalho4/Trabalho4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabalho4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalho4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
